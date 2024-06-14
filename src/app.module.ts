60150fb8ca794451b1023a19d32cc60b7e807114
bielgennaro <zirondigabriel@gmail.com>
2024-06-14 02:51:53 -0300

    feat: Add sales and sellers modules, entities, DTOs, services, and controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SellersModule } from './sellers/sellers.module';
+import { SalesModule } from './sales/sales.module';
 
 @Module({
-  imports: [],
+  imports: [SellersModule, SalesModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, {
     abortOnError: false,
     cors: true,
   });
+
+  const swaggerOptions = {
+    options: {
+      swaggerUiEnabled: true,
+      customSiteTitle: 'Xbrain API',
+    },
+  };
+
+  const config = new DocumentBuilder()
+    .setTitle('Xbrain API')
+    .setDescription('API para o teste da Xbrain')
+    .setVersion('1.0')
+    .addTag('xbrain')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, swaggerOptions.options);
+  new FastifyAdapter();
+
   await app.listen(3000);
 }
 bootstrap();
